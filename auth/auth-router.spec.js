9f5b360f7c9fbf6118fa86e49b73863c592c9294
Daniel Cruz <d.cruz.489@hotmail.com>
2020-02-21 22:04:37 -0700

    testing complete

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,28 @@
+const server = require('../api/server.js');
+const request = require('supertest');
+// const db = require('../database/dbConfig.js');
+
+describe('root', () => {
+  test('environment should be testing', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+});
+
+describe('GET /', () => {
+  it('should return 200 ok', async () => {
+    const res = await request(server)
+    .get('/');
+    
+    expect(res.status).toBe(200);
+  });
+
+  it('should be json', async () => {
+    const res = await request(server).get('/');
+    expect(res.type).toBe('application/json');
+  }),
+
+  it('should return the right object', async () => {
+    const res = await request(server).get('/');
+    expect(res.body).toEqual({ message: 'Welcome to your final Sprint Challenge this side of LABS!!!' });
+  });
+});
